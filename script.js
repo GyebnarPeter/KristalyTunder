030038ef9f2a895391dcf1803481240fc7e38c23
unknown <gyebnarpetter@gmail.com>
2020-07-23 16:55:08 +0200

    i have added the cover effect

--- script.js
+++ script.js
@@ -1,10 +1,67 @@
 // TOGGLE NAV
 let navIcon = document.querySelector(".nav-icon");
+let header = document.querySelector(".header-wrap");
 
 $(".nav-icon").click(() => {
     $(".header-wrap").slideToggle();
+
+    header.style.backgroundColor = "black";
 
     navIcon.classList.contains("nav-icon-rotate")
         ? navIcon.classList.remove("nav-icon-rotate")
         : navIcon.classList.add("nav-icon-rotate")
 });
+
+
+//NAVIGATION SCROLL
+let navi = document.querySelectorAll("nav a");
+
+window.onscroll = () => {
+
+    if (document.documentElement.scrollTop > 100) {
+
+        header.classList.add("header-background");
+        header.style.color = "black";
+
+        for (let i = 0; i < navi.length; ++i) {
+            navi[i].style.color = "black";
+        }
+        
+    } else {
+    
+        header.style.color = "white";
+        header.classList.remove("header-background");
+
+        for (let i = 0; i < navi.length; ++i) {
+            navi[i].style.color = "white";
+        }
+    }      
+};
+
+
+//COVER ANIMATION
+let firstWelcomeText = document.querySelector(".main-cover h1");
+let secondWelcomeText = document.querySelector(".main-cover h4");
+let coverLine = document.querySelector(".cover-text-underline");
+let coverButton = document.querySelector(".main-cover button");
+
+let coverAnimation = () => {
+
+    setTimeout( () => {
+        coverLine.style.opacity = 1;
+    }, 1000)
+
+    setTimeout( () => {
+        firstWelcomeText.style.opacity = 1;
+    }, 2000)
+
+    setTimeout( () => {
+        secondWelcomeText.style.opacity = 1;
+    }, 3000)
+
+    setTimeout( () => {
+        coverButton.style.opacity = 1;
+    }, 4000)
+}
+
+coverAnimation();
